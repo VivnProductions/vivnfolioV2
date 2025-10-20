602c5c2f4d860c3fbac232a3be8c45fb9f2545c9
VivnProductions <viviane.laoue@gmail.com>
2025-10-20 16:06:56 +0200

    Add viteStaticCopy to force redirects

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,9 +3,22 @@
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 import { defineConfig } from 'astro/config';
+import { viteStaticCopy } from 'vite-plugin-static-copy';
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://vivnproductions.netlify.app',
-	integrations: [mdx(), sitemap()],
+  site: 'https://vivnproductions.netlify.app',
+  integrations: [mdx(), sitemap()],
+  vite: {
+    plugins: [
+      viteStaticCopy({
+        targets: [
+          {
+            src: 'public/_redirects',
+            dest: './' // copie à la racine du dossier dist
+          }
+        ]
+      })
+    ]
+  }
 });
